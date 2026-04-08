5ae9c3d6542bc6604878009fbd8c484f307a06b7
kennedymahoro <kennedymahoro1@gmail.com>
2026-04-07 23:20:21 -0400

    added all elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
-
 export default function Home() {
   // Sart page content here
   return (
-    <main className="">
-        
+    <main className="flex flex-col items-center " style={{ backgroundColor: "#8A7863" }}>
+
+
+      <div className="flex flex-col items-center justify-center space-y-8 h-screen" style={{ backgroundColor: "#AD987D" }}>
+        <h1 className="text-4xl font-bold mb-4">Meristem</h1>
+
+      <div className="flex flex-col space-y-4">
+        <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> New Map</button>
+        <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Load Map</button>
+      </div>
+      </div>
+
     </main>
   );
 }
