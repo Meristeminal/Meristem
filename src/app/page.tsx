fb53907ebd2ce9ca77ddef3cb0ca3ecda5b32c83
kennedymahoro <kennedymahoro1@gmail.com>
2026-04-13 13:31:24 -0400

    finished the UI for the start page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   // Sart page content here
   return (
-    <main className="flex flex-col items-center " style={{ backgroundColor: "#8A7863" }}>
+    <main className="flex flex-col items-center h-screen w-screen" style={{ backgroundColor: "#8A7863" }}>
 
-
-      <div className="flex flex-col items-center justify-center space-y-8 h-screen" style={{ backgroundColor: "#AD987D" }}>
-        <h1 className="text-4xl font-bold mb-4">Meristem</h1>
-
-      <div className="flex flex-col space-y-4">
-        <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> New Map</button>
-        <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Load Map</button>
-      </div>
+      <div className="flex items-center justify-center h-full w-full">
+        <div className="flex flex-col items-center justify-center space-y-8 w-4/12 h-4/12 rounded-2xl" style={{ backgroundColor: "#AD987D" }}>
+          <h1 className="text-4xl font-bold mb-4">Meristem</h1>
+          <div className="flex flex-col space-y-4">
+            <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> New Map</button>
+            <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Load Map</button>
+          </div>
+        </div>
       </div>
 
     </main>
