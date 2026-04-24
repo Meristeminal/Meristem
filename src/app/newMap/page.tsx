3f0ebbd90a9c3ebc0ee761a4d52d7d535811bcaf
Achintya14 <achintyachaudhary14@gmail.com>
2026-04-24 13:31:16 -0400

    Removed the unnecessarily commented portion from src/app/newMap/page.tsx

--- src/app/newMap/page.tsx
+++ src/app/newMap/page.tsx
@@ -7,20 +7,3 @@ import MapEditor from "../../Components/MapEditor";
 export default function NewMapPage() {
   return <MapEditor />;
 }
-
-/*
-export default function NewMap() {
-  // New Map page content here
-
-    // from A to L
-    const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K","L"];
-    // to ten
-    const cols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  return (
-    <main className="flex flex-col items-center h-screen w-screen bg-[#8A7863]">
-      <GridBoard />
-      <ToolBar />
-    </main>
-  );
-}
-*/
